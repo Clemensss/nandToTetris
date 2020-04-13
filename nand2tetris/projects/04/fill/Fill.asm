599d035398f1e6156115bde777750e794ced62bc
Clemensss <cass@ic.ufal.br>
2020-04-13 18:26:55 -0300

    chapter 4 is done

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,103 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+    
+    @16384
+    D=A
+    @R0
+    M=D
+
+    @24575
+    D=A
+    @R1
+    M=D
+
+    @isitset
+    M=0
+
+(KEYLOOP)
+    @CHECK
+
+    @24576
+    D=M
+    @SETANDGO
+    D;JNE
+
+(CHECK)
+    @isitset
+    D=M
+
+    @RESET
+    D;JNE
+     
+    @SETANDGO
+    0;JMP
+
+(SETANDGO)
+    @isitset
+    M=1
+
+    @THING
+    0;JMP
+
+(THING)
+    //this is pretty for a retard
+    @32767
+    D=A
+
+    @R0
+    A=M
+
+    M=D
+
+    @R0
+    M=M+1
+    D=M
+    @R1
+    D=M-D
+
+    @THING
+    D;JGT
+
+    @RESETCOUNT
+    0;JMP
+
+(RESETCOUNT)
+    @16384
+    D=A
+    @R0
+    M=D
+
+    @KEYLOOP
+    0;JMP
+
+(RESET)
+    @isitset
+    M=0
+
+    @CLEAR
+    0;JMP
+
+(CLEAR)
+    @R0
+    A=M
+    M=0
+
+    @R0
+    M=M+1
+    D=M
+    @R1
+    D=M-D
+
+    @RESETCOUNT
+    D;JLE
+
+    @CLEAR
+    0;JMP
+
+   
+    
+
+
+
+
